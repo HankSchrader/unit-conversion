d060b8f9d240195ec323f38a2f1452938a8c7dd0
Erik Mikac <mikacew@gmail.com>
2021-01-12 18:51:10 -0500

    Initial Commit

---
+++ unit-conversion/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  unit-conversion
+//
+//  Created by Sara Nicole Mikac on 1/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ unit-conversion/unit_conversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  unit_conversionApp.swift
+//  unit-conversion
+//
+//  Created by Sara Nicole Mikac on 1/12/21.
+//
+
+import SwiftUI
+
+@main
+struct unit_conversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
